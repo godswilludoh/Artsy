52e1947abafb24908ee5fb68cf580ed2b0314013
Godswill Udoh <godswilludoh@gmail.com>
2023-01-06 15:18:52 +0100

    feat: Add top creators section

--- src/components/home/components/TopCreators.jsx
+++ src/components/home/components/TopCreators.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
 
 const TopCreators = () => {
-	return <div className='mt-10'>TopCreators</div>;
+	return (
+		<div className='mt-24 bg-[#E2E2E2]'>
+			<div className='w-10/12 px-20 pt-10 text-display font-extrabold text-3xl leading-loose text-[#161616]'>
+				TOP CREATORS OF <br /> THE WEEK
+			</div>
+			<p className='px-20 pt-10 text-lg text-[#000000] opacity-50 font-extralight'>
+				“Everything always looked better in black and white. Everything always
+				as if it were the first time; <br /> there’s always more people in a
+				black and white photograph. <br /> It just makes it seem that there were
+				more people at a gig, more people at a football match, than with colour
+				photography.
+				<br /> Everything looks more exciting.”– Jack Lowden
+			</p>
+		</div>
+	);
 };
 
 export default TopCreators;
